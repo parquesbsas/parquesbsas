6c074a1b93e442baf8ee4c2f71022f16e98a7378
Lenwe <nicoadiaz93@gmail.com>
2019-02-18 01:11:51 -0300

    Cambio base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/parquesbsas/';
+$config['base_url'] = 'https://parques-bsas.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
