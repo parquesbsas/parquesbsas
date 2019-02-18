0b2c40aa5f7f93dde69e57d3512cad1935e2900e
Lenwe <nicoadiaz93@gmail.com>
2019-02-18 00:55:09 -0300

    Configuracion base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
-	'database' => 'u757745325_web5',
+	//'hostname' => 'localhost',
+	//'username' => 'root',
+	//'password' => '',
+	//'database' => 'u757745325_web5',
+	'hostname' => $server,
+	'username' => $username,
+	'password' => $password,
+	'database' => $database,
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
