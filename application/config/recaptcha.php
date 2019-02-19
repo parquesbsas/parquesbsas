af33155faaa0f6f4382f7a45d804b072f69e4475
Victor <victor.paz@fizzmod.com>
2019-02-19 00:49:37 -0300

    key google catpcha, v2

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -1,6 +1,6 @@
 <?php
 
-$config['recaptcha_sitekey'] = "6LdOW5IUAAAAAN-QRcMLF-W5ieJSIcNr3rYtGV1L";
-$config['recaptcha_secretkey'] = "6LdOW5IUAAAAADWEDw1SLCdqDdiPwiK5Efj_thvm";
+$config['recaptcha_sitekey'] = "6LelW5IUAAAAAMjCTVhkrfzVt-SaLkD6c-Y7tsHR";
+$config['recaptcha_secretkey'] = "6LelW5IUAAAAAIA4OTl4Li2C96dDj0H-yQUNRLx6";
 $config['lang'] = "es";
 ?>
