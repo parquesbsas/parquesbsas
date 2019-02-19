4fdbe8b5790ce9a731925ac687ab23674a746026
Victor <victor.paz@fizzmod.com>
2019-02-19 01:14:49 -0300

    google login keys

--- application/config/google.php
+++ application/config/google.php
@@ -10,10 +10,10 @@
 |  api_key           string   Developer key.
 |  scopes            string   Specify scopes
 */
-$config['google']['client_id']        = "59440704155-v05r7agj9argmb7cqbfilhvl8pjllmhs.apps.googleusercontent.com";//"899455636819-iih3e7668kss33j2u32docbhsc1cj9fl.apps.googleusercontent.com";
-$config['google']['client_secret']    = "9lHs6VzNILHjVFzOvwb7zGFO";//'b6ZURD_qmU1u3_Np7E98fwfy';
-$config['google']['redirect_uri']     = 'http://localhost/parquesbsas/google_login';
-$config['google']['application_name'] = 'Parques Bs As Test';
+$config['google']['client_id']        = "349286300413-odgs25ijmv7dlg8bbm5ag8qfpbjgpimk.apps.googleusercontent.com";
+$config['google']['client_secret']    = "W7E_yXpMptIXplXqOLQ1QqGD";
+$config['google']['redirect_uri']     = 'https://parques-bsas.herokuapp.com/google_login';
+$config['google']['application_name'] = 'Parques Bs As';
 $config['google']['api_key']          = '';
 $config['google']['scopes']           = array();
 
